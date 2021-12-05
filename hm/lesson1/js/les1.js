d247b05ecc8a13bc1307ad9861a59571d219d7b8
natusyandr <natusyandr@gmail.com>
2021-12-05 22:30:34 +0300

    in progress

---
+++ hm/lesson1/js/les1.js
@@ -0,0 +1,47 @@
+"use strict";
+// let cats = [
+//        {
+//            name: "Люся",
+//            age: "1 год",
+//            color: "трехцветная",
+//            img: "https://picsum.photos/id/219/1000/1000"
+//        },
+//        {
+//            name: "Макс",
+//            age: 4,
+//            color: "серый",
+//            img: "https://picsum.photos/id/1074/1000/1000"
+//        },
+//        {
+//            name: "Василий",
+//            age: 1,
+//            color: "трехцветный",
+//            img: "https://picsum.photos/id/593/1000/1000"
+//        }
+//    ];
+
+Vue.createApp({
+  data(){
+    return {
+      cats: [
+             {
+                 name: "Люся",
+                 age: "1 год",
+                 color: "трехцветная",
+                 img: "https://picsum.photos/id/219/1000/1000"
+             },
+             {
+                 name: "Макс",
+                 age: 4,
+                 color: "серый",
+                 img: "https://picsum.photos/id/1074/1000/1000"
+             },
+             {
+                 name: "Василий",
+                 age: 1,
+                 color: "трехцветный",
+                 img: "https://picsum.photos/id/593/1000/1000"
+             }]
+    }
+  }
+}).mount("#app");
