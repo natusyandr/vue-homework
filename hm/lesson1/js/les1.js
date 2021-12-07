d6c42b3183977b7c1cacd917395ad1def539c360
natusyandr <natusyandr@gmail.com>
2021-12-07 15:20:27 +0300

    1 les done

--- hm/lesson1/js/les1.js
+++ hm/lesson1/js/les1.js
@@ -32,13 +32,13 @@ Vue.createApp({
              },
              {
                  name: "Макс",
-                 age: 4,
+                 age: "4 года",
                  color: "серый",
                  img: "https://picsum.photos/id/1074/1000/1000"
              },
              {
                  name: "Василий",
-                 age: 1,
+                 age: "1 год",
                  color: "трехцветный",
                  img: "https://picsum.photos/id/593/1000/1000"
              }]
